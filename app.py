993e9908d380cfcba44b2f3b28be71be332a8f7f
Justin Clayton <jt.clayton92@yahoo.com>
2020-07-07 16:59:08 -0700

    Setup wireframe html, css, and js. Add basic implementations of endpoints for navigating site.

--- app.py
+++ app.py
@@ -1,14 +1,41 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 from mysqlconnection import connectToMySQL
 
 app = Flask(__name__)
+app.secret_key = 'This is a big secret'
 
 @app.route('/')
 def index():
-  mysql = connectToMySQL('first_flask')
-  friends = mysql.query_db('SELECT * FROM friends')
-  print(friends)
+  # mysql = connectToMySQL('first_flask')
   return render_template('index.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+  session['user_fn'] = 'Justin'
+  session['user_id'] = 1
+  return redirect('/wall')
+
+@app.route('/register', methods=['POST'])
+def register():
+  return redirect('/wall')
+
+@app.route('/wall')
+def wall():
+  users = [
+    {'id': 1, 'first_name': 'Justin'},
+    {'id': 2, 'first_name': 'Lili'},
+    {'id': 3, 'first_name': 'Andres'}
+  ]
+  return render_template('wall.html', users=users)
+
+@app.route('/send_message')
+def send_message():
+  return redirect('/wall')
+
+@app.route('/logout')
+def logout():
+  session.clear()
+  return redirect('/')
 
 if (__name__ == '__main__'):
   app.run(debug=True)
--- static/js/index.js
+++ static/js/index.js
@@ -6,8 +6,36 @@
 (function() {
   $(document).ready(init);
 
-  /** Initialize the JS to control page behavior */
+  /** Initialize the JS to control page behavior. */
   function init() {
-    console.log('init');
+    setupBtns();
+    $('#pw_confirm').on('input', onCheckPW);
+  }
+
+  /** Validate that pws match before sending to registration. */
+  function onCheckPW() {
+    if ($(this).val() === $('#pw').val()) {
+      this.setCustomValidity('');
+    } else {
+      this.setCustomValidity('Passwords must match.');
+    }
+  }
+
+  /** Swap to login/registration page. */
+  function onSwapRegistration() {
+    $('#register').toggleClass('hidden');
+    $('#login').toggleClass('hidden');
+  }
+
+  /** Setup up buttons to swap between register and login. */
+  function setupBtns() {
+    $('#swap-login').click((e) => {
+      e.preventDefault();
+      onSwapRegistration();
+    });
+    $('#swap-registration').click((e) => {
+      e.preventDefault();
+      onSwapRegistration();
+    });
   }
 })();
